374fd63d0680eef38c10e0fcf5b65bd343bf762a
abdy <abdyn.7@gmail.com>
2025-06-07 17:35:35 -0600

    JS Fetch Buscar Pokemon

---
+++ pokedex/script.js
@@ -0,0 +1,14 @@
+// Buscar datos del Pokemon dependiendo de su número o nombre
+
+function buscarPokemon(contenedorNumero){
+    let inputId = `pokemonInput${contenedorNumero}`;
+    let nombrePokemon = document.getElementById(inputId).value.trim().toLowerCase();
+    let urlApi = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`
+
+
+    fetch(urlApi)
+    .then(Response => Response.json())
+    .then(datosPokemon => mostrarPokemon(datosPokemon, contenedorNumero))
+    .catch(() => mostrarError(contenedorNumero))
+}
+
